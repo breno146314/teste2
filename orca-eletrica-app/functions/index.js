1aa2d6afe667c8cc58ba7853d7c81a4d077c364b
breno146314 <breno2459@gmail.com>
2025-06-08 21:55:50 -0300

    erros index.js

--- orca-eletrica-app/functions/index.js
+++ orca-eletrica-app/functions/index.js
@@ -1,7 +1,8 @@
-const { onRequest } = require("firebase-functions/v2/https"); // <- Aqui o espaço é aceitável, é desestruturação
+const {onRequest} = require("firebase-functions/v2/https");
 const logger = require("firebase-functions/logger");
 
 exports.minhaPrimeiraFuncaoHTTP = onRequest((request, response) => {
-  logger.info("Hello logs!", { structuredData: true }); // <- Aqui está o erro: { structuredData: true }
-  response.send("Olá do Firebase! Esta é sua primeira função HTTP v2.");
+  logger.info("Hello logs!", {structuredData: true});
+  response.send("Olá do Firebase! Esta é sua primeira função HTTP v2.");
 });
+
