0495200e2ef3d3f6588a4e9b640e6a5c5c12279c
breno146314 <breno2459@gmail.com>
2025-06-13 20:39:52 -0300

    exclusao

--- orca-eletrica-app/functions/index.js
+++
@@ -1,8 +0,0 @@
-const {onRequest} = require("firebase-functions/v2/https");
-const logger = require("firebase-functions/logger");
-
-exports.minhaPrimeiraFuncaoHTTP = onRequest((request, response) => {
-  logger.info("Hello logs!", {structuredData: true});
-  response.send("Olá do Firebase! Esta é sua primeira função HTTP v2.");
-});
-
